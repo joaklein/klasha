7542946354cf28a73049859921182c8f65d223ff
Josh Klein <skiz303@gmail.com>
2023-02-22 17:29:37 -0500

    first commit for github

---
+++ script.js
@@ -0,0 +1,28 @@
+let xValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let yValues = [6, 4.5, 5, 3, 2.8, 3, 2, 4, 3, 2.2, 5.5, 5, 3.2, 3.5, 6];
+
+new Chart("myChart", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "rgba(247,18,60,1.0)",
+            borderColor: "rgba(247,18,60,1.0)",
+            data: yValues,
+            pointRadius: 0,
+        }]
+    },
+    options: {
+        responsive: true,
+        legend: { display: false },
+        scales: {
+            yAxes: [{
+                ticks: { min: 0, max: 8 },
+                display: false
+            }],
+            xAxes: [{ display: false }]
+        }
+    }
+});
